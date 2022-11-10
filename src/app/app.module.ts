f3cd7b0b278b4da5281d4ac566b7fd06bcfa0802
Wizari <wizaripost@gmail.com>
2022-11-10 18:45:25 +0300

    add sidenav container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './view/app.component';
+import {MainFrameComponent} from './main-frame/main-frame.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatSliderModule} from "@angular/material/slider";
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainFrameComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/main-frame/main-frame.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-frame',
+  templateUrl: './main-frame.component.html',
+  styleUrls: ['./main-frame.component.scss']
+})
+export class MainFrameComponent implements OnInit {
+  showFiller = false;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
